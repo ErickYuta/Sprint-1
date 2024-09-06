823c437399a41b757fb5511aa554b53ffc982d9c
ErickYuta <erick.miyagi@sptech.school>
2024-09-06 11:48:49 -0300

    adicionar comentarios

--- arduinoTCRT5000.ino
+++ arduinoTCRT5000.ino
@@ -1,18 +1,18 @@
-const int PINO_SENSOR_TCRT5000 = 7; // número da entrada digital
+const int PINO_SENSOR_TCRT5000 = 7; // número da porta digital
 
-void setup() {
-  pinMode(PINO_SENSOR_TCRT5000, INPUT); // configura o pino como entrada (INPUT)
+void setup() { //sketch que inicializa após iniciar a aplicação, local onde armazena input, output e variáveis
+  pinMode(PINO_SENSOR_TCRT5000, INPUT); // configura o pino como entrada (INPUT) || pinMode(PINO,MODE)
   Serial.begin(9600); // velocidade de leitura de bits por segundo
 }
 
 void loop() {
-  int estadoSensor = digitalRead(PINO_SENSOR_TCRT5000); // leitura do valor que o sensor está devolvendo
+  int estadoSensor = digitalRead(PINO_SENSOR_TCRT5000); // leitura do valor digital retornando HIGH ou LOW  || digitalRead(pino)
 
-  if (estadoSensor == LOW) {
+  if (estadoSensor == LOW) { // 1 = True
     Serial.println("Objeto Detectado"); // se tem objeto mostra essa mensagem
-  } else {
+  } else { // 0 = False
     Serial.println("Nenhum Objeto Detectado");  // se não tem objeto mostra essa mensagem
   }
 
-  delay(1000); // cria um atraso de 1 segundo
+  delay(1000); // cria um atraso de 1 segundo 1
 }
