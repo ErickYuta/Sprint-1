2c709dbd4785442c9a0a51cd9c680f97117d7d37
Vlad-Ribeiro <vlad.ribeiro@sptech.school>
2024-09-06 12:53:33 -0300

    vlad IDE 3

--- arduinoTCRT5000.ino
+++ arduinoTCRT5000.ino
@@ -1,6 +1,7 @@
 const int PINO_SENSOR_TCRT5000 = 7; // número da entrada digital
   int ativado = 0; // variável global para a presença do carro
   int desativado = 0; // variável global para vaga vazia
+  int const separacao = 0;
 
 void setup() { // inicia a configuração
   pinMode(PINO_SENSOR_TCRT5000, INPUT); // configura o pino como entrada (INPUT)
@@ -9,15 +10,33 @@ void setup() { // inicia a configuração
 
 void loop() { // inica a contagem das variáveis
   int estadoSensor = digitalRead(PINO_SENSOR_TCRT5000); // leitura do valor que o sensor está devolvendo
+  String p1=";";
 
   if (estadoSensor == LOW) {
-    ativado++; // incrementa o valor quando tem carro
-    Serial.println(ativado); // se tem objeto mostra essa mensagem
     desativado = 0; // zera o valor de desativado quando sair um carro
+    ativado++; // incrementa o valor quando tem carro
+
+    Serial.print("Separação:");
+    Serial.print(separacao);
+    Serial.print(p1);
+    Serial.print("Tempo Ativado:");
+    Serial.print(ativado);
+    Serial.print(p1);
+    Serial.print("Tempo Desativado:");
+    Serial.println(desativado);
+
   } else {
-    desativado--; // incrementa o valor quando não tem carro
-    Serial.println(desativado);  // se não tem objeto mostra essa mensagem
     ativado = 0; // zera o valor do ativado
+    desativado--; // incrementa o valor quando não tem carro
+    delay(1000);
+    Serial.print("Separação:");
+    Serial.print(separacao);
+    Serial.print(p1);
+    Serial.print("Tempo Ativado:");
+    Serial.print(ativado);
+    Serial.print(p1);
+    Serial.print("Tempo Desativado:");
+    Serial.println(desativado);
   }
 
   delay(1000); // cria um atraso de 1 segundo
